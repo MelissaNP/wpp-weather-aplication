7197fd9d18f6b991facb40e9929648f3fbdc9dee
Melissa Pereira <melissa2pereira@gmail.com>
2024-08-25 18:27:03 -0300

    fix: weather service not connecting with weather api

--- src/main/java/com/springdemo/demo/WeatherService.java
+++ src/main/java/com/springdemo/demo/WeatherService.java
@@ -1,13 +1,36 @@
 package com.springdemo.demo;
 
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WeatherService {
 
+    private static final String API_URL = "http://api.weatherstack.com/current?access_key=4b609c3eeb1f57b25df9898e7dc935d1&query=";
+    
+    public String getWeatherMessage(String location) {
+            RestTemplate restTemplate = new RestTemplate();
+            String url = API_URL + location;
+            String response = restTemplate.getForObject(url, String.class);
+            
+            JSONObject jsonResponse = new JSONObject(response);
+            
+            if (jsonResponse.has("error")) {
+                return "Unable to fetch weather data. Please try again later.";
+            }
+            
+            JSONObject currentWeather = jsonResponse.getJSONObject("current");
+            
+            String temperature = currentWeather.get("temperature").toString();
+            String weatherDescription = currentWeather.getJSONArray("weather_descriptions").getString(0);
+            String humidity = currentWeather.get("humidity").toString();
+            
+            return String.format("Current weather in %s:\nTemperature: %s°C\nCondition: %s\nHumidity: %s%%",
+                    location, temperature, weatherDescription, humidity);
+        }
+
+/*
     @Value("${weatherstack.api_key}")
     private String weatherApiKey;
 
@@ -30,6 +53,5 @@ public class WeatherService {
         return "Current weather in " + location + ":\n" +
                 "Temperature: " + temperature + "°C\n" +
                 "Condition: " + weatherDescription + "\n" +
-                "Humidity: " + humidity + "%";
+                "Humidity: " + humidity + "%";*/
     }
-}
