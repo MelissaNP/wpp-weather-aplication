170395a34e84e6381eb2fc27c5bae6ffd5b422f9
Melissa Pereira <melissa2pereira@gmail.com>
2024-08-25 17:29:56 -0300

    removing unused database credentials

--- src/main/java/com/springdemo/demo/Whatsapp.java
+++ src/main/java/com/springdemo/demo/Whatsapp.java
@@ -1,18 +1,14 @@
 package com.springdemo.demo;
 
-import jakarta.persistence.*;
 import lombok.Data;
 
 import java.io.Serializable;
 import java.util.UUID;
 
-@Entity
-@Table(name = "WppNumbers")
+
 @Data
 public class Whatsapp implements Serializable {
     
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID idProduct;
     private String phoneNumber;
     
